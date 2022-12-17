302c40d6036d600a721e99d4f9af391786aedfc1
Madi <85469257+Daniel-Dunavetsky@users.noreply.github.com>
2022-12-16 23:49:27 -0800

    Added clothes picker
    Added a way to choose how many and what the clothes is along if it is a top or a bottom

--- Clothes.java
+++ Clothes.java
@@ -3,5 +3,21 @@ import java.util.ArrayList;
 public class Clothes 
 {
     private String clothes;
-    private ArrayList<String> clothesList = new ArrayList<String>();
+    private String type;
+
+    //multi-arg
+    public Clothes(String clothes, String type)
+    {
+        this.clothes = clothes;
+        this.type = type;
+    }
+
+    //toString
+    public String toString()
+    {
+        String output = clothes + ", " + type;
+
+        return output;
+    }
+
 }
--- Main.java
+++ Main.java
@@ -1,10 +1,35 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main
 {
-    public static void main(String arg) 
+    public static void main(String[] arg) 
     {
         Scanner sc = new Scanner(System.in);
         
+        ArrayList<Clothes> clothesList = new ArrayList<Clothes>();
+
+        System.out.println("How many items are there?");
+        int n = sc.nextInt();
+
+        int i = 0;
+        
+        //asks user to pick clothes
+        while(i < n)
+        {
+            System.out.println("What is the name of the item? ");
+            String name = sc.next();
+
+            System.out.println("What is the type of the item? ");
+            String type = sc.next();
+
+            Clothes c = new Clothes(name, type);
+
+            clothesList.add(c);
+
+            System.out.println("\n");
+            i++;
+        }
+
     }
 }
